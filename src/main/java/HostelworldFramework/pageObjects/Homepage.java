5354af0c305f0281ed9a7166dc1b666f2bc0cb0f
Joseph Lalor <joseph.lalor@gmail.com>
2016-06-10 14:46:50 +0100

    Removed redundant imports from Homepage.java

--- src/main/java/HostelworldFramework/pageObjects/Homepage.java
+++ src/main/java/HostelworldFramework/pageObjects/Homepage.java
@@ -7,15 +7,12 @@
 package HostelworldFramework.pageObjects;
 
 import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Homepage
-//public class Homepage
 {	
 	WebDriver driver;
 	WebDriverWait wait;
